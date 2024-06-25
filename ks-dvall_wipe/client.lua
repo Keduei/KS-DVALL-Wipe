601fc8ed38fb81349c4d5a01f500842713c04270
Keduei <moonrptr@gmail.com>
2024-06-26 02:54:19 +0300

    Add files via upload

---
+++ ks-dvall_wipe/client.lua
@@ -0,0 +1,19 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+RegisterNetEvent("kedue:dvall")
+AddEventHandler("kedue:dvall", function()
+    local vehicles = GetGamePool('CVehicle')
+    for k, vehicle in pairs(vehicles) do
+        if (not IsPedAPlayer(GetPedInVehicleSeat(vehicle, -1))) then 
+            SetVehicleHasBeenOwnedByPlayer(vehicle, false) 
+            SetEntityAsMissionEntity(vehicle, false, false) 
+            DeleteVehicle(vehicle)
+            DeleteEntity(vehicle)
+            if (DoesEntityExist(vehicle)) then 
+                DeleteVehicle(vehicle) 
+                DeleteEntity(vehicle)
+            end
+        end
+    end
+end)
+
---
+++ ks-dvall_wipe/fxmanifest.lua
@@ -0,0 +1,12 @@
+
+fx_version 'cerulean'
+games { 'gta5' }
+author 'Kedue'
+description 'DV ALL + Wipe'
+client_scripts {
+    'client.lua'
+}
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
+}
